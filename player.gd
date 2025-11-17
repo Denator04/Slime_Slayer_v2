d88ffeaab6186344d1cc0064ace551ffce905b46
TheMisterBanann <szymon12gut@gmail.com>
2025-11-18 00:15:00 +0100

    cos
    cos

--- player.gd
+++ player.gd
@@ -1,44 +1,99 @@
 extends CharacterBody2D
 
-@onready var sprite = $AnimatedSprite2D
+@onready var sprite = $spriteOffset/AnimatedSprite2D
+@onready var sprite_offset = $spriteOffset 
 
 @export var movement_speed: float = 500
 @export var run_multiplier: float = 2
 @export var flip_distance_multiplier: float = 1.2 
 
+
 var character_direction: Vector2 = Vector2.ZERO
 var is_flipping: bool = false
 var player_chosen = 1
+var direction = 0
 
+var is_attacking
+var moveOnAttack: Vector2 = Vector2.ZERO
+
+
+var attack_an
 var idle_an
 var walk_animation: String
 
+
+#		*****	This defines direction mc goes towards	*****
 func player_walk(a, b, c, d):
 	if character_direction.x > 0:
 		walk_animation = a
+		direction = 3
 	elif character_direction.x < 0:
 		walk_animation = b
+		direction = 1
 	elif character_direction.y > 0:
 		walk_animation = c
+		direction = 2
 	elif character_direction.y < 0:
 		walk_animation = d
+		direction = 0
+		
+		
+# 		******		direction variable related	 	*******
+func direction_depend():
+	if(direction == 0):
+		idle_an = "rycerz_idle_back_B"
+		attack_an = "attack_up_B"
+		moveOnAttack = Vector2(0, 20)
+	elif(direction == 1):
+		idle_an = "rycerz_idle_side_B"
+		attack_an = "attack_side_B"
+		moveOnAttack = Vector2(20, 0)
+	elif (direction == 2):
+		idle_an = "rycerz_idle_front_B"
+		attack_an = "attack_down_B"
+		moveOnAttack = Vector2(0, -20)
+	elif(direction == 3):
+		idle_an = "rycerz_idle_side_B"
+		attack_an = "attack_side_B"
+		moveOnAttack = Vector2(-20, 0)
 
+
+#		*****	This defines animations depending on which character was chosen		*****
 func player_stats() -> void:
 	if player_chosen == 1:
-		idle_an = "rycerz_idle_B"
-		player_walk("walk_side_B", "walk_side_B", "walk_front_B", "walk_back_B")
+		idle_an = "rycerz_idle_front_B"
 		
+#	**************		ATTACK function		***************
+#	**************							***************
+func attack():
+	is_attacking = true
+	sprite_offset.position = moveOnAttack
+	sprite.play(attack_an)
+	
+	await sprite.animation_finished
+	is_attacking = false
+	#sprite_offset.position = Vector2.ZERO
+	is_attacking = false
 		
 func _ready():
+	sprite.z_index = 10 
 	player_stats()
 	
 
 func _physics_process(delta):
-	player_stats()
+
+	direction_depend()
+	
+	if is_attacking:
+		return
 	
 	if is_flipping:
 		move_and_slide()
 		return
+	
+	
+
+
 
 	character_direction.x = Input.get_axis("ui_left", "ui_right")
 	character_direction.y = Input.get_axis("ui_up", "ui_down")
@@ -56,13 +111,20 @@ func _physics_process(delta):
 			sprite.flip_h = true
 		elif character_direction.x > 0:
 			sprite.flip_h = false
-
+			
+			
 		if !is_flipping and character_direction != Vector2.ZERO:
+			player_walk("walk_side_B", "walk_side_B", "walk_front_B", "walk_back_B")
 			sprite.play(walk_animation)
+			
 	else:
 		velocity = Vector2.ZERO
 		if sprite.animation != idle_an and not is_flipping and character_direction == Vector2.ZERO:
 			sprite.play(idle_an)
+			
+	#	*****	Here is attack handled		*****
+	if Input.is_action_just_pressed("ui_c"):
+		attack()
 
 	if Input.is_action_just_pressed("ui_space") and not is_flipping:
 		start_flip()
@@ -92,5 +154,3 @@ func _on_flip_finished() -> void:
 
 	if character_direction != Vector2.ZERO:
 		sprite.play(walk_animation)
-	else:
-		sprite.play(idle_an)
