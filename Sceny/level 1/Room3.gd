2eac6ae84d755ee7ee1180408aeaf0810478630a
Denator04 <129318333+Denator04@users.noreply.github.com>
2026-01-12 21:01:08 +0100

    .

--- Sceny/level 1/Room3.gd
+++ Sceny/level 1/Room3.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
 func _ready():
-	if Global.spawn_point_name != "" and has_node(Global.spawn_point_name):
+	if Global.spawn_point_name == "":
+		Global.spawn_point_name = "default"
+
+	if has_node(Global.spawn_point_name):
 		$Player.global_position = get_node(Global.spawn_point_name).global_position
